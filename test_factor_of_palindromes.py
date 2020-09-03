ffe1ce97acf5933dab3d2700d3240cf4f71e20aa
Pashkov <a.pashkov@bk.ru>
2020-09-03 13:40:06 +0300

    Factor_of_palindromes V1

---
+++ test_factor_of_palindromes.py
@@ -0,0 +1,30 @@
+import unittest
+import factor_of_palindromes as p
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.data_false = 'abcde'
+        self.data_false_reversed = 'edcba'
+        self.data_true_1 = '1abcba1'
+        self.data_true_2 = 'abcbab'
+
+    def test_reverse_string(self):
+        self.assertEqual(p.reverse_string(self.data_false), self.data_false_reversed)
+
+    def test_check_palindrome(self):
+        self.assertTrue(p.check_palindrome(self.data_true_1))
+        self.assertFalse(p.check_palindrome(self.data_false))
+
+    def test_find_palindrome(self):
+        self.assertEqual(p.find_palindrome(self.data_true_1), 'bcb')
+        self.assertEqual(p.find_palindrome(self.data_true_2), 'bab')
+
+    '''
+    def test_something(self):
+        self.assertEqual(True, False)
+
+'''
+if __name__ == '__main__':
+    unittest.main()
